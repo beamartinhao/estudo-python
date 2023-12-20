7027d94817a89da77dcfef801ddcc6db8bcd339f
Bia <soueubianalista@gmail.com>
2023-12-20 11:07:20 -0300

    Starting

---
+++ 01_introducao_python.py
@@ -0,0 +1,23 @@
+#aula 1 - comentarios
+#código se lê de esquerda pra direita cima pra baixo
+
+
+
+#aula 2 - docstring
+
+"""
+um bloco de texto
+sem ter que colocar # como comentario toda vez que começar
+"""
+
+#função print, usada para exibir algo na tela
+#argumento - algo que você coloca para funções, pode ser argumento nomeado
+
+print(1,2, sep='-',end='?')
+#sep='' é um separador, tudo o que colocar dentro do separador vai separar os argumentos
+#end='', final de linha, pode colocar algo ou pular linha
+
+
+
+
+
---
+++ 02_estruturas_de_dados.py
@@ -0,0 +1,29 @@
+"""
+python = tipagem dinâmica (já sabe qual o tipo de informação do argumento)/ forte
+
+string -> textos
+textos só podem ficar entre aspas
+"""
+#tipos de aspas que funcionam com tipo string
+print("Beatriz")
+print('Beatriz')
+
+
+"""
+tipos int e float 
+int - numero inteiro, negativo ou positivo 
+"""
+#tipo int
+print(11)
+print(0)
+print(-11)
+
+"""
+tipo float  -> numero com ponto flutuante 
+número quebrado 
+"""
+#tipo float
+print(1.1)
+
+x = 1.1
+print(type(x))
